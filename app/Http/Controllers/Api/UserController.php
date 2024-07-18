59c13a861136a903c03a7e6e16d15a167a69a86d
matutaj <matutajorge2002@gmail.com>
2024-07-18 23:51:28 +0100

    add

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use App\Models\User;
 
 class UserController extends Controller
 {
-    public function index(){
+    public function index() :JsonReponse{
+        //Pegando os usuários do banco de dados
         $users = User::orderBy("id", "DESC")->paginate(2);
 
+        //Retornando os dados como JSON
         return response()->json([
             "status"=>true,
             "users"=> $users,
