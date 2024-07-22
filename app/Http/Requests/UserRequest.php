6d1ff4937eaa0c2b8e19ae311185524e22627e6e
matutaj <matutajorge2002@gmail.com>
2024-07-22 11:21:52 +0100

    fx: change file nama

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -5,6 +5,8 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
+
+
 class UserResquest extends FormRequest
 {
     /**
@@ -28,14 +30,7 @@ class UserResquest extends FormRequest
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
 
-     protected function failedValidation(Validator $validator){
-        throw new HttpResponseException(response()->json([
-
-            "status"=> false,
-            "erros"=>$validator ->errors()
-        ],422));
-    }
-
+   
 
     public function rules(): array
     {
@@ -63,6 +58,14 @@ class UserResquest extends FormRequest
     }
 
     //Manipular Falha de validação e retornar uma resposta JSON com os erros Válidos
+    protected function failedValidation(Validator $validator){
+        throw new HttpResponseException(response()->json([
+
+            "status"=> false,
+            "erros"=>$validator ->errors()
+        ],422));
+    }
+
 
   
 }
