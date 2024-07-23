f50f2902e23a10b05c1c15fccdcb94b2c6aca14e
matutaj <matutajorge2002@gmail.com>
2024-07-23 08:40:43 +0100

    Add categoria route

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Usercontroller;
+use App\Htpp\Controllers\Api\CategoriaController;
 
-
+//Rotas do Usuário
 Route::get("/users", [UserController::class, "index"]); //Traz todos os dados de forma paginada
 Route::get("/users/{user}", [UserController::class, "show"]);// Lista ou apresenta apenas o usuário pelo o Id do usuário.
 Route::post("/users", [UserController::class, "store"]);
 Route::put("/users/{user}", [UserController::class, "update"]);
+
+//Rotas da Categoria
+Route::post("/categoria", [CategoriaController::class, "store"]);
