a4f957325dd7bb9562d71402213dbd49a84438e6
matutaj <matutajorge2002@gmail.com>
2024-07-28 10:35:14 +0100

    Add views

---
+++ resources/views/emails/sendEmailTextForgotPasswordCode.blade.php
@@ -0,0 +1,7 @@
+ Prezado(a) {{$user->name}}
+    
+ Para recuperar a sua senha do aplicativo de Livraria, use o código de verificação abaixo:
+
+{{$code}}
+    
+ Por questões de segurança esse código é válido somente até as {{$formattedTima}} do dia {{$formattedDate}}. Caso esse Prazo esteja expirado, será necessário solicitar outro código
