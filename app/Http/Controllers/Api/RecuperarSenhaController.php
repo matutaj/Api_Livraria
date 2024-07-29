be050695d68bcc8a6d8c281cdec51e68baef6249
matutaj <matutajorge2002@gmail.com>
2024-07-29 10:27:01 +0100

    feat: add reset code password

--- app/Http/Controllers/Api/RecuperarSenhaController.php
+++ app/Http/Controllers/Api/RecuperarSenhaController.php
@@ -17,8 +17,9 @@ use Illuminate\Support\Facades\Mail;
 class RecuperarSenhaController extends Controller
 {
     public function forgotPasswordCode(ForgotPasswordRequest $request){
+        
 
-        $user =User::where("email", $request->email)->first();
+        $user = User::where("email", $request->email)->first();
 
         if(!$user){
 
@@ -93,4 +94,163 @@ class RecuperarSenhaController extends Controller
         ],400);
         }
     }
+ /**
+     * Validar o código de recuperação de senha enviado pelo usuário.
+     *
+     * Este método valida o código de recuperação de senha enviado pelo usuário.
+     * Utiliza o serviço ResetPasswordValidateCodeService para validar o código. Se o código for válido,
+     * retorna uma resposta de sucesso. Caso contrário, retorna uma resposta de erro.
+     *
+     * @param ResetPasswordValidateCodeRequest $request O request contendo o e-mail e o código de recuperação de senha
+     * @param ResetPasswordValidateCodeService $ResetPasswordValidateCodeService O serviço utilizado para validar o código de recuperação de senha
+     * Injeção de Dependência: o Laravel automaticamente resolve e injeta uma instância dessa classe no método quando é chamado.
+     * @return \Illuminate\Http\JsonResponse Resposta indicando sucesso ou falha na validação do código
+     */
+    public function resetPasswordValidateCode(ResetPasswordValidateCodeRequest $request, ResetPasswordValidateCodeService $resetPasswordValidateCode): JsonResponse
+    {
+
+        try{
+
+            // Validar o código do token
+            $validationResult = $resetPasswordValidateCode->resetPasswordValidateCode($request->email, $request->code);
+
+            // Verificar o resultado da validação
+            if(!$validationResult['status']){
+
+                // Exibir mensagem de erro
+                return response()->json([
+                    'status' => false,
+                    'message' => $validationResult['message'],
+                ], 400);
+
+            }
+
+            // Recuperar os dados do usuário
+            $user = User::where('email', $request->email)->first();
+
+            // Verificar existe o usuário no banco de dados
+            if(!$user){
+                
+                // Salvar log
+                Log::notice('Usuário não encontrado.', ['email' => $request->email]);
+
+                // Exibir mensagem de erro
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Usuário não encontrado!',
+                ], 400);
+
+            }
+                
+            // Salvar log
+            Log::info('Código recuperar senha válido.', ['email' => $request->email]);
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Código recuperar senha válido!',
+            ], 200);
+
+        } catch (Exception $e){
+
+            // Salvar log
+            Log::warning('Erro validar código recuperar senha.', ['email' => $request->email, 'error' => $e->getMessage()]);
+
+            return response()->json([
+                'status' => false,
+                'message' => 'Código inválido!',
+            ], 400);
+        }
+        
+    }
+
+    /**
+     * Resetar a senha do usuário com base no código de recuperação.
+     *
+     * Este método resetar a senha do usuário com base no código de recuperação enviado pelo usuário.
+     * Utiliza o serviço ResetPasswordValidateCodeService para validar o código. Se o código for válido, atualiza a senha
+     * do usuário no banco de dados e retorna uma resposta de sucesso com o token de acesso JWT.
+     * Caso contrário, retorna uma resposta de erro.
+     *
+     * @param ResetPasswordCodeRequest $request O request contendo o e-mail, o código de recuperação de senha e a nova senha
+     * @param ResetPasswordValidateCodeService $resetPasswordValidateCode O serviço utilizado para validar o código de recuperação de senha
+     * Injeção de Dependência: o Laravel automaticamente resolve e injeta uma instância dessa classe no método quando é chamado.
+     * @return \Illuminate\Http\JsonResponse Resposta indicando sucesso ou falha na resetar da senha do usuário
+     */
+    public function resetPasswordCode(ResetPasswordCodeRequest $request, ResetPasswordValidateCodeService $resetPasswordValidateCode): JsonResponse
+    {
+
+        try{
+
+            // Validar o código do token
+            $validationResult = $resetPasswordValidateCode->resetPasswordValidateCode($request->email, $request->code);
+
+            // Verificar o resultado da validação
+            if(!$validationResult['status']){
+                
+                // Exibir mensagem de erro
+                return response()->json([
+                    'status' => false,
+                    'message' => $validationResult['message'],
+                ], 400);
+
+            }
+
+            // Recuperar os dados do usuário
+            $user = User::where('email', $request->email)->first();
+
+            // Verificar existe o usuário no banco de dados
+            if(!$user){
+                
+                // Salvar log
+                Log::notice('Usuário não encontrado.', ['email' => $request->email]);
+
+                // Exibir mensagem de erro
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Usuário não encontrado!',
+                ], 400);
+
+            }
+
+            // Alterar a senha do usuário no banco de dados
+            $user->update([
+                'password' => Hash::make($request->password)
+            ]);
+
+            // gerar o token 
+            $token = $user->first()->createToken('api-token')->plainTextToken;
+
+            // Recuperar os registros recuperar senha do usuário
+            $userPasswordResets = DB::table('password_reset_tokens')->where('email', $request->email);
+
+            // Se existir token cadastrado para o usuário recuperar senha, excluir o mesmo
+            if($userPasswordResets){
+                $userPasswordResets->delete();
+            }
+
+            // Salvar log
+            Log::info('Senha atualizada com sucesso.', ['email' => $request->email]);
+
+            return response()->json([
+                'status' => true,
+                'user' => $user,
+                'token' => $token,
+                'message' => 'Senha atualizada com sucesso!',
+            ], 200);
+        }catch(Exception $e){
+
+            // Salvar log
+            Log::warning('Senha não atualizada.', ['email' => $request->email, 'error' => $e->getMessage()]);
+
+            return response()->json([
+                'status' => false,
+                'message' => 'Senha não atualizada!',
+            ], 400);
+
+        }
+        
+    }
+
+
+
 }
