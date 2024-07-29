734918a32615e3016fd5c995a6094f4719ec3d39
matutaj <matutajorge2002@gmail.com>
2024-07-29 16:42:23 +0100

    feat: add method store controller livro

---
+++ app/Http/Controllers/Api/LivroController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Livro;
+use Illuminate\Support\Facades\DB;
+use Exception;
+
+class LivroController extends Controller
+{
+    //
+    public function index(){
+
+    }
+
+    public function store(Request $request){
+        DB::beginTransaction();
+
+        try{
+            $livro = Livro::create([
+                "nome"=>$request->nome,
+                "descricao"=>$request->descricao,
+                "preco"=>$request->preco,
+                "edicao"=>$request->edicao,
+                "autor"=>$request->autor,
+                "dataLancamente"=>$request->dataLancamento,
+                "quantidade"=>$request->quantidade,
+                "imagem"=>$request->imagem,
+            ]);
+            DB::commit();
+
+            return response()->json([
+                "status"=>true,
+                "message"=> "Salvo com sucesso",
+            ], 201);
+        }catch( Exception $e){
+            DB::rollBack();
+
+            return response()->json([
+                "status"=>false,
+                "message"=>"Erro ao salvar o livro",
+            ], 400);
+        }
+    }
+}
