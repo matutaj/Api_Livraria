f724f62631368f32870c5c0ae15093a2aeeb2d02
matutaj <matutajorge2002@gmail.com>
2024-07-31 11:01:18 +0100

    add fillable

--- app/Models/Livro.php
+++ app/Models/Livro.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Livro extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        "nome",
+        "descricao",
+        "preco",
+        "edicao",
+        "autor",
+        "dataLancamento",
+        "quantidade",
+        "imagem",
+        "categoriaId",
+    ];
 }
