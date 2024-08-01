333374be6a901f2596016a771a34cadc66e15299
matutaj <matutajorge2002@gmail.com>
2024-08-01 11:33:18 +0100

    feat: rollback on migrate

--- database/migrations/2024_07_31_214733_create_venda_table.php
+++ database/migrations/2024_07_31_214733_create_venda_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('venda', function (Blueprint $table) {
+        Schema::create('vendas', function (Blueprint $table) {
             $table->id();
             $table->integer("quantidade");
             $table->double("valorPago");
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('venda');
+        Schema::dropIfExists('vendas');
     }
 };
