2bde7ce5a4c2f08e3aa40134fbbe8646027459bf
matutaj <matutajorge2002@gmail.com>
2024-08-01 11:00:24 +0100

    feat: add controller method store

--- app/Http/Controllers/Api/VendaController.php
+++ app/Http/Controllers/Api/VendaController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Venda;
+use Illuminate\support\Facades\DB;
+use Exception;
 
 class VendaController extends Controller
 {
@@ -16,5 +18,30 @@ class VendaController extends Controller
             "status"=> true,
             "vendas"=>$vendas,
         ], 200);
+    }
+
+    public function store(Request $request){
+        DB::beginTransaction();
+
+        try{
+            $venda = Venda::create($request->all());
+            DB::commit();
+
+            return response()->json([
+                "status"=>true,
+                "venda"=> $venda,
+                "message"=>"Salvo com sucesso",
+            ], 201);
+
+        }catch( Exception $e){
+            DB::rollBack();
+
+            return response()->json([
+                "status"=>false,
+                "message"=>"Erro ao cadastrar a venda",
+                "Error"=> $e->getMessage(),
+            ], 400);
+        }
+
     }
 }
