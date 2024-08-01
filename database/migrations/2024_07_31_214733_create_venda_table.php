2e34b8318b59d02c58336fb172d77f4be96d24f9
matutaj <matutajorge2002@gmail.com>
2024-08-01 10:34:45 +0100

    feat: create venda migration

---
+++ database/migrations/2024_07_31_214733_create_venda_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('venda', function (Blueprint $table) {
+            $table->id();
+            $table->integer("quantidade");
+            $table->double("valorPago");
+            $table->foreignId("userId")->references("id")->on("users")->onDelete("cascade")->onUpdate("cascade");
+            $table->foreignId("livroId")->references("id")->on("livros")->onDelete("cascade")->onUpdate("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('venda');
+    }
+};
